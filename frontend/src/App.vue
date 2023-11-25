7acb250867e0838bbe5b099dae471138deed738f
VISI <109953146+5isi@users.noreply.github.com>
2023-11-25 21:44:24 +0800

    add main text field and button.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,40 @@
 
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <h1>Trojan-Wails</h1>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-text-field label="Remote Server Address" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-text-field label="Remote Server Port" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-text-field label="Password" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-text-field label="SNI" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-text-field label="Bind Local Port" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-btn color="primary">Start Trojan</v-btn>
+          <v-btn color="secondary" class="ml-5">Kill Trojan</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
